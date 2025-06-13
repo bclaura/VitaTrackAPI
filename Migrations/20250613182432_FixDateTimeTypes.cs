8a0129b621e689f4f3acb303f7afd2cc853cb976
bclaura <124773748+bclaura@users.noreply.github.com>
2025-06-13 21:25:54 +0300

    ...

---
+++ Migrations/20250613182432_FixDateTimeTypes.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VitaTrackAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDateTimeTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "users",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "timestamp",
+                table: "sensor_data",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start_time",
+                table: "physical_activities",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "end_time",
+                table: "physical_activities",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "sent_at",
+                table: "messages",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "recorded_at",
+                table: "location_map",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "timestamp",
+                table: "ecg_signals",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "recorded_at",
+                table: "chart_data",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "alarms",
+                type: "timestamp without time zone",
+                nullable: true,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldNullable: true,
+                oldDefaultValueSql: "(getdate())");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "users",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "timestamp",
+                table: "sensor_data",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "start_time",
+                table: "physical_activities",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "end_time",
+                table: "physical_activities",
+                type: "datetime",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "sent_at",
+                table: "messages",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "recorded_at",
+                table: "location_map",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "timestamp",
+                table: "ecg_signals",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "recorded_at",
+                table: "chart_data",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "alarms",
+                type: "datetime",
+                nullable: true,
+                defaultValueSql: "(getdate())",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true,
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
